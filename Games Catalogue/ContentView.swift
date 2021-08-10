9098794c1c698fb0258b07ab850e7e6b0d0530ba
fajarazay <fajarseptian.dev@gmail.com>
2021-08-11 02:20:29 +0700

    initial commit

---
+++ Games Catalogue/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Games Catalogue
+//
+//  Created by Fajar Septian on 11/08/21.
+//  Copyright ©2021 Fajar Septian. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
