90126cd59b40b4c5bd01c883a104e88d75482f85
fajarazay <fajar.septian@quipper.com>
2021-09-01 22:08:47 +0700

    add shimmer image placeholder and hide text if data  is empty

--- Games Catalogue/Module/Detail/View/DetailView.swift
+++ Games Catalogue/Module/Detail/View/DetailView.swift
@@ -21,150 +21,148 @@ struct DetailView: View {
             ScrollView(.vertical, showsIndicators: false) {
                 
                 VStack(alignment: .leading ) {
-                    if self.detailPresenter.loadingState {
-                        Spacer()
-                        Loader()
-                        Spacer()
-                    } else {
-                        if !self.detailPresenter.loadingState {
+                    VStack {
+                        WebImage(url: URL(string: detailPresenter.game.backgroundImage))
+                            .resizable()
+                            .placeholder {
+                                ShimmerView()
+                            }
+                            .indicator(.activity)
+                            .transition(.fade(duration: 0.5))
+                            .scaledToFit()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(height: 320)
+                        
+                        ZStack(alignment: .topTrailing) {
                             
-                            VStack {
-                                WebImage(url: URL(string: detailPresenter.game.backgroundImage))
-                                    .resizable()
-                                    .indicator(.activity)
-                                    .transition(.fade(duration: 0.5))
-                                    .scaledToFit()
-                                    .aspectRatio(contentMode: .fill)
-                                    .frame(height: 320)
+                            VStack(alignment: .leading) {
+                                Text(detailPresenter.game.name)
+                                    .font(.title)
+                                    .fontWeight(.bold)
                                 
-                                ZStack(alignment: .topTrailing) {
-                                    
-                                    VStack(alignment: .leading) {
-                                        Text(detailPresenter.game.name)
-                                            .font(.title)
-                                            .fontWeight(.bold)
-                                        
-                                        HStack(alignment: .top) {
-                                            Text(detailPresenter.game.genres.reduce(
-                                                    "", { ($0.isEmpty ? "" : $0 + ", ") + $1.name!
-                                                    }))
-                                                .font(.system(size: 14))
-                                                .fontWeight(.bold)
-                                        }.padding(.top, 8)
-                                        
-                                        Text((detailPresenter.game.releasedDate).getFormattedDate)
-                                            .foregroundColor(Color.gray)
+                                HStack(alignment: .top) {
+                                    Text(detailPresenter.game.genres.reduce(
+                                            "", { ($0.isEmpty ? "" : $0 + ", ") + $1.name!
+                                            }))
+                                        .font(.system(size: 14))
+                                        .fontWeight(.bold)
+                                }.padding(.top, 8)
+                                
+                                Text((detailPresenter.game.releasedDate).getFormattedDate)
+                                    .foregroundColor(Color.gray)
+                                    .font(.system(size: 16))
+                                    .padding(.vertical, 16)
+                                
+                                if !detailPresenter.loadingState && !detailPresenter.game.publishers.isEmpty {
+                                    HStack(alignment: .center) {
+                                        Text("Publishers")
                                             .font(.system(size: 16))
-                                            .padding(.vertical, 16)
-                                        
-                                        HStack(alignment: .center) {
-                                            Text("Publishers")
-                                                .font(.system(size: 16))
-                                                .fontWeight(.bold)
-                                            Text(detailPresenter.game.publishers.reduce(
-                                                    "", { ($0.isEmpty ? "" : $0 + ", ") + $1.name!
-                                                    }))
-                                                .font(.system(size: 16))
-                                        }.padding(.bottom, 16)
-                                        
-                                        Text(detailPresenter.game.description)
-                                            .lineLimit(self.isExpanded ? nil : 2)
-                                        
-                                        Button {
-                                            self.isExpanded.toggle()
-                                        } label: {
-                                            Text(self.isExpanded ? "Show Less..." : "Show More...")
-                                                .foregroundColor(hexColor(0x00BFA6))
-                                                .font(.system(size: 16)).bold()
-                                                .padding(.top, 4)
-                                                .background(Color.white)
-                                        }.frame(alignment: .leading)
-                                        
-                                        Group {
-                                            Text("Platforms")
-                                                .font(.system(size: 16))
-                                                .fontWeight(.bold)
-                                                .padding(.top, 24)
-                                            
-                                            VStack(alignment: .leading) {
-                                                generateContent(in: geometry, data: detailPresenter.game.platforms)
-                                                
-                                            }.padding(.all, -8)
-                                        }
-                                        
-                                        VStack(alignment: .leading) {
-                                            Text("Reviews & Ratings")
-                                                .font(.system(size: 16))
-                                                .fontWeight(.bold)
-                                            
-                                            HStack {
-                                                Text(String(detailPresenter.game.rating))
-                                                    .font(.system(size: 54))
-                                                
-                                                VStack(alignment: .leading) {
-                                                    RatingView(rating: detailPresenter.game.rating)
-                                                    HStack {
-                                                        Text("\(detailPresenter.game.reviewsCount) reviews")
-                                                            .font(.system(size: 16))
-                                                            .foregroundColor(Color.gray)
-                                                        Spacer()
-                                                    }
-                                                }
-                                            }
-                                        }.padding(.top, 24)
-                                        
-                                        Group {
-                                            VStack(alignment: .leading) {
-                                                Text("Available on ")
-                                                    .font(.system(size: 16))
-                                                    .fontWeight(.bold)
-                                                    .padding(.bottom, 16)
-                                                
-                                                ForEach(detailPresenter.game.stores) { store in
-                                                    Button {
-                                                        let urlDomain = String(describing: store.domain)
-                                                        if let url = URL(
-                                                            string: "\(Constants.prefixUrlDomain)\(urlDomain)") {
-                                                            UIApplication.shared.open(url)
-                                                        }
-                                                    } label: {
-                                                        Text(store.name ?? "")
-                                                            .foregroundColor(ColorsManager.primary)
-                                                            .font(.system(size: 16))
-                                                            .fontWeight(.bold)
-                                                    }.padding(.bottom, 16)
-                                                }
-                                            }.padding(.top, 16)
-                                        }
-                                        
-                                    }.padding(.horizontal, 16)
-                                    .padding(.top, 32)
-                                    .background(Color.white)
-                                    .clipShape(RoundedShapeView())
-                                    .padding(.top, -48)
+                                            .fontWeight(.bold)
+                                        Text(detailPresenter.game.publishers.reduce(
+                                                "", { ($0.isEmpty ? "" : $0 + ", ") + $1.name!
+                                                }))
+                                            .font(.system(size: 16))
+                                    }.padding(.bottom, 16)
+                                }
+                                
+                                if !detailPresenter.loadingState && !detailPresenter.game.description.isEmpty {
+                                    Text(detailPresenter.game.description)
+                                        .lineLimit(self.isExpanded ? nil : 2)
                                     
                                     Button {
-                                        if detailPresenter.isFavoriteGame {
-                                            detailPresenter.setUnfavorite(game: detailPresenter.game)
-                                        } else {
-                                            detailPresenter.setFavorite(game: detailPresenter.game)
-                                        }
-                                    } label: { Image(systemName: detailPresenter.isFavoriteGame
-                                                        ? "heart.fill" : "heart")
-                                        .foregroundColor(.white)
-                                        .font(.system(size: 24))
-                                        .padding()
-                                        .background(ColorsManager.primary)
-                                        .clipShape(Circle())
-                                        .shadow(color: Color.gray, radius: 12)
-                                        
-                                    }.padding(.top, -72)
-                                    .padding(.horizontal, 32)
+                                        self.isExpanded.toggle()
+                                    } label: {
+                                        Text(self.isExpanded ? "Show Less..." : "Show More...")
+                                            .foregroundColor(hexColor(0x00BFA6))
+                                            .font(.system(size: 16)).bold()
+                                            .padding(.top, 4)
+                                            .background(Color.white)
+                                    }.frame(alignment: .leading)
                                 }
-                            }
-                            Spacer()
+                                
+                                Group {
+                                    Text("Platforms")
+                                        .font(.system(size: 16))
+                                        .fontWeight(.bold)
+                                        .padding(.top, 24)
+                                    
+                                    VStack(alignment: .leading) {
+                                        generateContent(in: geometry, data: detailPresenter.game.platforms)
+                                        
+                                    }.padding(.all, -8)
+                                }
+                                
+                                VStack(alignment: .leading) {
+                                    Text("Reviews & Ratings")
+                                        .font(.system(size: 16))
+                                        .fontWeight(.bold)
+                                    
+                                    HStack {
+                                        Text(String(detailPresenter.game.rating))
+                                            .font(.system(size: 54))
+                                        
+                                        VStack(alignment: .leading) {
+                                            RatingView(rating: detailPresenter.game.rating)
+                                            HStack {
+                                                Text("\(detailPresenter.game.reviewsCount) reviews")
+                                                    .font(.system(size: 16))
+                                                    .foregroundColor(Color.gray)
+                                                Spacer()
+                                            }
+                                        }
+                                    }
+                                }.padding(.top, 24)
+                                
+                                Group {
+                                    VStack(alignment: .leading) {
+                                        Text("Available on ")
+                                            .font(.system(size: 16))
+                                            .fontWeight(.bold)
+                                            .padding(.bottom, 16)
+                                        
+                                        ForEach(detailPresenter.game.stores) { store in
+                                            Button {
+                                                let urlDomain = String(describing: store.domain)
+                                                if let url = URL(
+                                                    string: "\(Constants.prefixUrlDomain)\(urlDomain)") {
+                                                    UIApplication.shared.open(url)
+                                                }
+                                            } label: {
+                                                Text(store.name ?? "")
+                                                    .foregroundColor(ColorsManager.primary)
+                                                    .font(.system(size: 16))
+                                                    .fontWeight(.bold)
+                                            }.padding(.bottom, 16)
+                                        }
+                                    }.padding(.top, 16)
+                                }
+                                
+                            }.padding(.horizontal, 16)
+                            .padding(.top, 32)
+                            .background(Color.white)
+                            .clipShape(RoundedShapeView())
+                            .padding(.top, -48)
+                            
+                            Button {
+                                if detailPresenter.isFavoriteGame {
+                                    detailPresenter.setUnfavorite(game: detailPresenter.game)
+                                } else {
+                                    detailPresenter.setFavorite(game: detailPresenter.game)
+                                }
+                            } label: { Image(systemName: detailPresenter.isFavoriteGame
+                                                ? "heart.fill" : "heart")
+                                .foregroundColor(.white)
+                                .font(.system(size: 24))
+                                .padding()
+                                .background(ColorsManager.primary)
+                                .clipShape(Circle())
+                                .shadow(color: Color.gray, radius: 12)
+                                
+                            }.padding(.top, -72)
+                            .padding(.horizontal, 32)
                         }
                     }
+                    Spacer()
                 }.frame(minWidth: geometry.size.width, minHeight: geometry.size.height, maxHeight: .infinity)
             }.frame(minWidth: geometry.size.width, minHeight: geometry.size.height, maxHeight: .infinity)
         }.onAppear {
